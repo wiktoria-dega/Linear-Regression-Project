255dc8ba80e3c8ead43fdcf1ccab620c72769ab9
wiktoria-dega <wikidg4@gmail.com>
2024-08-29 22:31:10 +0200

    Update geo analysis functions

--- house_price_geo_analysis.py
+++ house_price_geo_analysis.py
@@ -8,45 +8,6 @@ import math
 from locations_coords import LOCATIONS
 from conv_calculation import conv_factor_kc
 
-df_houseprice = pd.read_csv(r"C:\Users\Wiktoria\Desktop\Python Basics\Projekt\Regresja liniowa\kc_house_data.csv")
-
-df_houseprice['sqft_living'].isna().any()
-df_houseprice['sqft_living'].isnull().any()
-
-df_houseprice['sqft_living'] = df_houseprice['sqft_living'].fillna(df_houseprice['sqft_living'].median())
-
-df_houseprice['sqft_living'].isnull().any()
-df_houseprice['sqft_living'].isna().any()
-
-df_houseprice['price'].isna().any()
-df_houseprice['price'].isnull().any()
-
-df_houseprice['price'] = df_houseprice['price'].fillna(df_houseprice['price'].median())
-
-df_houseprice['price'].isna().any()
-df_houseprice['price'].isnull().any()
-
-df_houseprice['price_by_sqft'] = df_houseprice['price'] / df_houseprice['sqft_living']
-
-df_houseprice['price_by_sqft'].isnull().any()
-
-df_houseprice['price_by_sqft'].describe()
-
-#Downtown Seattle
-seattle_downtown = (47.6062, -122.3321)
-
-#Bellevue City
-bellevue_city = (47.6104, -122.2007)
-
-#Northwest Seattle
-northwest_seattle = (47.6495, -122.4045)
-
-locations = {
-    'dist_to_downtown': seattle_downtown,
-    'dist_to_bellevue': bellevue_city,
-    'dist_to_northwest_seattle': northwest_seattle
-    }
-
 
 def calc_manhattan_dist(lat1, long1, lat2, long2):
     return abs(lat2 - lat1) + abs(long2 - long1)
@@ -73,36 +34,37 @@ def analyze_dist_col(df, columns, conv_factor_kc):
 
     return df
 
-df_houseprice = add_distance_columns(df_houseprice, LOCATIONS)
-
-columns_to_convert = ['dist_to_downtown', 'dist_to_bellevue', 'dist_to_northwest_seattle']
-
-df_houseprice = analyze_dist_col(df_houseprice, columns_to_convert, conv_factor_kc)
-
-bins_km = [0.0, 5.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0]
-labels = ['0-5 km', '5-10 km', '10-20 km', '20-30 km', '30-40 km', '40-50 km',
-          '50-60 km', '60-70 km', '70-80 km', '80-90 km']
-
-for col in ['dist_to_downtown_km', 'dist_to_bellevue_km', 'dist_to_northwest_seattle_km']:
-    df_houseprice[f'{col}_bins'] = pd.cut(df_houseprice[col], bins=bins_km, labels=labels)
+def plot_dist_analysis(df):
     
-    location_name = col.replace('_km', '').replace('dist_to_', '').replace('_', ' ').title()
+    bins_km = [0.0, 5.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0]
+    labels = ['0-5 km', '5-10 km', '10-20 km', '20-30 km', '30-40 km', '40-50 km',
+              '50-60 km', '60-70 km', '70-80 km', '80-90 km']
     
-    plt.figure()
-    sns.barplot(x=f'{col}_bins', y='price_by_sqft', data=df_houseprice)
-    plt.title(f'Price by square foot by distance to {location_name}')
-    plt.xlabel('Distances')
-    plt.ylabel('Price by Square Foot')
-    plt.xticks(rotation=45)
-    plt.show()
-    
-    plt.figure()
-    sns.barplot(x=f'{col}_bins', y='price', data=df_houseprice)
-    plt.title(f'Price by distance to {location_name}')
-    plt.xlabel('Distances')
-    plt.ylabel('Price by Square Foot')
-    plt.xticks(rotation=45)
-    plt.show()
+    for col in ['dist_to_downtown_km', 'dist_to_bellevue_km', 'dist_to_northwest_seattle_km']:
+        df[f'{col}_bins'] = pd.cut(df[col], bins=bins_km, labels=labels)
+        
+        location_name = col.replace('_km', '').replace('dist_to_', '').replace('_', ' ').title()
+        
+        plt.figure()
+        sns.barplot(x=f'{col}_bins', y='price_by_sqft', data=df)
+        plt.title(f'Price by square foot by distance to {location_name}')
+        plt.xlabel('Distances')
+        plt.ylabel('Price by Square Foot')
+        plt.xticks(rotation=45)
+        plt.show()
+        
+        plt.figure()
+        sns.barplot(x=f'{col}_bins', y='price', data=df)
+        plt.title(f'Price by distance to {location_name}')
+        plt.xlabel('Distances')
+        plt.ylabel('Price by Square Foot')
+        plt.xticks(rotation=45)
+        plt.show()
+        
+    return df
+
+
+
 
 
 
