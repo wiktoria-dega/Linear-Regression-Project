4a7f321de1f2527fcedbebf8fe11d6f9b72ac0af
wiktoria-dega <wikidg4@gmail.com>
2024-09-02 08:19:49 +0200

    Improve models results

--- house_price_regression_model.py
+++ house_price_regression_model.py
@@ -7,12 +7,14 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
 
 client = MongoClient('mongodb://localhost:27017/')
 
-db = client.testowa_regresja
+db = client.test_nowe_kol
 
-collection = db.test
+collection = db.test_nowe
 
 data = list(collection.find())
 
@@ -21,7 +23,6 @@ df_houseprice = pd.DataFrame(data)
 df_houseprice.head()
 
 
-#split between test and training data
 X_train, X_test, y_train, y_test = train_test_split(
     df_houseprice.drop(columns=['price_log']),
     df_houseprice['price_log'],
@@ -29,7 +30,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     test_size=0.3)
 
 
-#data scaling
 scaler = MinMaxScaler()
 
 X_train = X_train.drop(columns=['_id'])
@@ -38,15 +38,15 @@ X_test = X_test.drop(columns=['_id'])
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-#linear regression
+#linear regression model
 model = LinearRegression()
 
-#training
+
 model.fit(X_train_scaled, y_train)
 
 pred = model.predict(X_test_scaled)
 
-#model evaluation
+
 print(f'MSE error for test data: {mean_squared_error(np.exp(y_test), np.exp(pred))}')
 print(f'RMSE error for test data: {mean_squared_error(np.exp(y_test), np.exp(pred), squared=False)}')
 print(f'R2-score: {r2_score(np.exp(y_test), np.exp(pred))}')
@@ -63,7 +63,7 @@ tree_reg.fit(X_train_scaled, y_train)
 
 pred_tree = tree_reg.predict(X_test_scaled)
 
-#model evaluation
+
 print(f'MSE error for test data: {mean_squared_error(np.exp(y_test), np.exp(pred_tree))}')
 print(f'RMSE error for test data: {mean_squared_error(np.exp(y_test), np.exp(pred_tree), squared = False)}')
 print(f'R2-score: {r2_score(np.exp(y_test), np.exp(pred_tree))}')
@@ -73,6 +73,7 @@ plt.scatter(list(range(len(y_test))), np.exp(y_test))
 plt.scatter(list(range(len(pred))), np.exp(pred))
 plt.legend(['real', 'pred_tree'])
 
+# importance of features
 for i,j in zip(tree_reg.feature_importances_, X_train.columns):
     print(i, j)
     
@@ -80,31 +81,27 @@ plt.figure()
 plt.barh(X_train.columns, tree_reg.feature_importances_)
 
 
-# Las losowy
-from sklearn.ensemble import RandomForestRegressor
-
+#random forest model
 rnd_forest = RandomForestRegressor(n_estimators=200,
                                    max_depth=5,
                                    random_state=123)
 
-# Trening
+
 rnd_forest.fit(X_train_scaled, y_train)
 
 pred_forest = rnd_forest.predict(X_test_scaled)
 
-# Ocena modelu
+
 print(f'MSE error for test data: {mean_squared_error(np.exp(y_test), np.exp(pred_forest))}')
 print(f'RMSE error for test data: {mean_squared_error(np.exp(y_test), np.exp(pred_forest), squared = False)}')
 print(f'R2-score: {r2_score(np.exp(y_test), np.exp(pred_forest))}')
 
-# Waznosci cech
+# importance of features
 for i,j in zip(rnd_forest.feature_importances_, X_train.columns):
     print(i, j)
 
 
-# Grid search
-from sklearn.model_selection import GridSearchCV
-
+#GridSearchCV
 params = {
     'n_estimators': [5, 10, 25, 40, 50, 55, 60],
     'max_depth': [None, 1, 2, 3, 4, 5, 6, 7],
@@ -115,13 +112,11 @@ rnd_forest_ = RandomForestRegressor()
 
 gs = GridSearchCV(rnd_forest_, params, scoring='r2', verbose=2, cv=2)
 
-# Trening
 gs.fit(X_train_scaled, y_train)
 
-# Sprawdzenie - predykcja najlepszego modelu
 pred_best = gs.best_estimator_.predict(X_test_scaled)
 
-# Ocena modelu
+
 print(f'MSE error for test data: {mean_squared_error(np.exp(y_test), np.exp(pred_best))}')
 print(f'RMSE error for test data: {mean_squared_error(np.exp(y_test), np.exp(pred_best), squared = False)}')
 print(f'R2-score: {r2_score(np.exp(y_test), np.exp(pred_best))}')
