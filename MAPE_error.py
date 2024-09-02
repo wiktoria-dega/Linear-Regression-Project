7c51af6c1d1bf21a3fbf8be2d70358ba9bf4b1c2
wiktoria-dega <wikidg4@gmail.com>
2024-09-02 22:51:48 +0200

    Add MAPE error to analyze two models

---
+++ MAPE_error.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def mean_absolute_percentage_error(y_test, y_pred):
+    y_test, y_pred = np.array(y_test), np.array(y_pred)
+    return np.mean(np.abs((y_test-y_pred) / y_test)) * 100
